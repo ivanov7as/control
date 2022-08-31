18222f72e02e55de4b0495defc71b08ef089e57f
IAS <ivanov7as@gmail.com>
2022-08-31 22:53:42 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,36 +1,59 @@
-﻿Console.WriteLine("Введите трехзначное число");
+﻿string palindrom()
+{
+Console.WriteLine("Введите пятизначное число");
 int num1 = Convert.ToInt32 (Console.ReadLine());
-num1 = (num1 / 10)%10;
-Console.WriteLine ("Второе число " + num1);
+int digit1 = num1/10000;
+int digit2 = num1/1000%10;
+int digit4 = num1/10%10;
+int digit5 = num1%1000%100%10;
+string answer = "";
 
-Console.WriteLine("Введите число");
-int num2 = Convert.ToInt32 (Console.ReadLine());
-if (num2 < 100){
-    Console.WriteLine("Третьего числа нет");
- }
-else
+if (digit1 == digit5 && digit2 == digit4)
 {
-while (num2>1000){
-    num2 = num2/10;
-}
-
-int thirdNumber;
-thirdNumber = (num2 % 100)%10;
-Console.WriteLine("Третья цифра " + thirdNumber);
-}
-Console.WriteLine("Ведите цифрру");
-int num3 = Convert.ToInt32 (Console.ReadLine());
-
-if (num3 <= 5)
-{
-    Console.WriteLine("День рабочий");
-}
-else if (num3 <=7)
-{
-    Console.WriteLine("День выходной");
+    answer = ("Число является палиндромом");
 
 }
 else
 {
-    Console.WriteLine("Введите правильную цифру");
+    answer = ("Число не является палиндромом");
 }
+return answer;
+}
+Console.WriteLine(palindrom());
+
+string distance()
+{
+Console.Write("Введите координату x первой точки ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату у первой точки ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z первой точки ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x второй точки ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y второй точки ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z второй точки ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+string truedistance = ("Расстояние между точками в 3D пространстве " + length);
+return truedistance;
+}
+Console.WriteLine(distance());
+
+string cubetable()
+{
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32 (Console.ReadLine());
+int counter = 1;
+List<int> table = new List<int>{};
+while (counter <= n)
+{
+    int cube = (int)Math.Pow(counter, 3);
+    counter+=1;
+    table.Add(cube);
+}
+string tablecube = String.Join(" ",table.ToArray());
+return tablecube;
+}
+Console.Write(cubetable());
