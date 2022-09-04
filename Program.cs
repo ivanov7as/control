3b679647b1347c86a0acb739912bf6e23b0a3c7c
IAS <ivanov7as@gmail.com>
2022-09-05 00:58:48 +0300

    4

--- Program.cs
+++ Program.cs
@@ -1,59 +1,88 @@
-﻿string palindrom()
+﻿string asqrtb(){
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32 (Console.ReadLine());
+int c = a;
+for (int i=1; i < b; i++)
+    {
+        a=a*c;
+    }
+string answer = ("A в степени B равно " + a);
+return answer;
+}
+Console.WriteLine(asqrtb());
+
+string summofnum()
 {
-Console.WriteLine("Введите пятизначное число");
+Console.WriteLine("Введите число");
 int num1 = Convert.ToInt32 (Console.ReadLine());
-int digit1 = num1/10000;
-int digit2 = num1/1000%10;
-int digit4 = num1/10%10;
-int digit5 = num1%1000%100%10;
-string answer = "";
-
-if (digit1 == digit5 && digit2 == digit4)
+int sum = 0;
+while (num1>0)
 {
-    answer = ("Число является палиндромом");
+    sum = sum + num1%10;
+    num1 = num1/10;
+}
+string summof =  ("Сумма чисмел " + sum);
+return summof;
+}
+Console.WriteLine(summofnum());
 
+stringarray();
+void stringarray()
+{
+int [] array = new int[8];
+for (int i = 0; i <array.Length; i++)
+{
+    Console.Write("Введите элемент " + (i+1) + " ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    
+}
+for (int i = 0; i <array.Length; i++)
+{
+    System.Console.Write("{0} ", array[i]);
+}
+}
+
+median();
+void median()
+{
+Console.WriteLine("Введите размерность массива N");
+int n = Convert.ToInt32 (Console.ReadLine());
+int [] arr = new int[n];
+int temp;
+double med;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = i; j < arr.Length; j++)
+    {
+        if (arr[i] > arr[j])
+        {
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+ 
+Console.WriteLine("Отсортированный массив");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
+Console.WriteLine("Медианное значение");
+if (n%2 == 0)
+{
+    med = (arr[((n-1)/2)]+arr[n/2])/2;
+    Console.WriteLine(med);    
 }
 else
 {
-    answer = ("Число не является палиндромом");
+    med = arr[(n/2)];
+    Console.WriteLine(med);
 }
-return answer;
 }
-Console.WriteLine(palindrom());
-
-string distance()
-{
-Console.Write("Введите координату x первой точки ");
-double x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату у первой точки ");
-double y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату z первой точки ");
-double z1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату x второй точки ");
-double x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату y второй точки ");
-double y2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату z второй точки ");
-double z2 = Convert.ToDouble(Console.ReadLine());
-double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-string truedistance = ("Расстояние между точками в 3D пространстве " + length);
-return truedistance;
-}
-Console.WriteLine(distance());
-
-string cubetable()
-{
-Console.WriteLine("Введите число n");
-int n = Convert.ToInt32 (Console.ReadLine());
-int counter = 1;
-List<int> table = new List<int>{};
-while (counter <= n)
-{
-    int cube = (int)Math.Pow(counter, 3);
-    counter+=1;
-    table.Add(cube);
-}
-string tablecube = String.Join(" ",table.ToArray());
-return tablecube;
-}
-Console.Write(cubetable());
