c182e2f042bbd3398ca99f06f2b906b346b2696f
IAS <ivanov7as@gmail.com>
2022-10-04 19:42:54 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,57 +1,18 @@
-﻿void nnumber()
+﻿string[] array1;
+Console.WriteLine("Введите массив строк");
+string arrayenter = Console.ReadLine();
+array1 = arrayenter.Split(' ');
+string[] array2 = new string[array1.Length];
+void NewArray(string[] array1, string[] array2)
 {
-Console.WriteLine("Задайте значение M:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте значение N:");
-int n = Convert.ToInt32(Console.ReadLine());
-NaturalNumber(m, n);
-void NaturalNumber(int m, int n)
-{
-  if (m > n) return;
-  NaturalNumber(m, n - 1);
-  Console.Write(n + ", ");
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[i] = array1[i];
+        }
+    }
 }
-}
-nnumber();
-Console.WriteLine();
-void nsum()
-{
-Console.WriteLine("Задайте значение M:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте значение N:");
-int n = Convert.ToInt32(Console.ReadLine());
-void NaturalSum(int m, int n, int sum)
-{
-  sum = sum + n;
-  if (n <= m)
-  {
-    Console.Write("Cумма натуральных элементов в промежутке от M до N равна " + (sum-m) );
-    return;
-  }
-  NaturalSum(m, n - 1, sum);
-}
-NaturalSum(m, n, m);
-}
-nsum();
-Console.WriteLine();
-void ackermannfunction()
-{
-Console.WriteLine("Задайте значение M:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте значение N:");
-int n = Convert.ToInt32(Console.ReadLine());
-int Ackermann(int n, int m)
-{
-	if(n == 0)
-	{
-		return m + 1;
-	}
-	if(n > 0 && m == 0)
-	{
-		return Ackermann(n - 1, 1);
-	}
-	return Ackermann(n - 1, Ackermann(n, m - 1));
-}
-Console.WriteLine("Функция Аккермана равна " + Ackermann(m,n));
-}
-ackermannfunction();
+
+NewArray(array1, array2);
+PrintArray(array2);
