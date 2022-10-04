8725666a4490b2d0f81281706a96f015ef340984
IAS <ivanov7as@gmail.com>
2022-10-04 19:50:28 +0300

    3

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ void NewArray(string[] array1, string[] array2)
         }
     }
 }
-
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 NewArray(array1, array2);
 PrintArray(array2);
