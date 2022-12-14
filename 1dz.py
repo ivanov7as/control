a40a68b9570474ea8d7f7cc2d55586665a90949d
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2022-12-14 21:45:46 +0300

    Update 1dz.py

--- 1dz.py
+++ 1dz.py
@@ -3,73 +3,53 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
-        class Solution1 {
-            public String mergeAlternately(String word1, String word2) {
-                int l1 = word1.length();
-                int l2 = word2.length();
-                if (l1 > l2) {
-                    l1 = l2;
+        class SolutionMaxAreaOfIsland {
+
+            public int maxAreaOfIsland(int[][] grid) {
+                int max = 0;
+                for (int i = 0; i < grid.length; ++i) {
+                    for (int j = 0; j < grid[0].length; ++j) {
+                        max = Math.max(max, dfs(grid, i, j));
+                    }
                 }
-                StringBuilder output = new StringBuilder();
-                for (int i = 0; i < l1; ++i) {
-                    output.append(word1.charAt(i));
-                    output.append(word2.charAt(i));
-                }
-                return output.append(word1.substring(l1)).append(word2.substring(l1)).toString();
+                return max;
+            }
+
+            public int dfs(int[][] grid, int i, int j) {
+                if (i < 0 || i == grid.length || j < 0 || j == grid[0].length) return 0;
+                if (grid[i][j] != 1) return 0;
+                grid[i][j] = 0;
+                return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j + 1) + dfs(grid, i, j - 1);
+            }
+        }
+        class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+
+            TreeNode() {
+            }
+
+            TreeNode(int val) {
+                this.val = val;
+            }
+
+            TreeNode(int val, TreeNode left, TreeNode right) {
+                this.val = val;
+                this.left = left;
+                this.right = right;
             }
         }
 
-        class Solution4 {
-            public String reverseWords(String s) {
-                StringBuilder sb = new StringBuilder();
-                int end = s.length() - 1;
-                while (end >= 0) {
-                    if (s.charAt(end) == ' ') {
-                        end -= 1;
-                        continue;
-                    }
-                    int start = end - 1;
-                    while (start >= 0 && s.charAt(start) != ' ') {
-                        start -= 1;
-                    }
-                    sb.append(" ");
-                    sb.append(s.substring(start + 1, end + 1));
-                    end = start - 1;
-                }
-                if (sb.length() > 0) {
-                    sb.deleteCharAt(0);
-                }
-                return sb.toString();
-            }
-        }
-
-        class Solution5 {
-            public boolean isPalindrome(String s) {
-                for (int left = 0, right = s.length() - 1; left < right; left++, right--) {
-                    while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
-                        left++;
-                    }
-                    while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
-                        right--;
-                    }
-                    if (left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-
-        class Solution2 {
-            public int removeElement(int[] nums, int val) {
-                int n = 0;
-                for (int i : nums)
-                    if (i != val)
-                        nums[n++] = i;
-                return n;
+        class SolutionIsSymmetric {
+            public boolean isSymmetric(TreeNode root) {
+                return isSymmetric(root, root);
             }
 
+            private boolean isSymmetric(TreeNode p, TreeNode q) {
+                if (p == null || q == null) return p == q;
+                return p.val == q.val && isSymmetric(p.left, q.right) && isSymmetric(p.right, q.left);
+            }
         }
     }
 }
