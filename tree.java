8139d3690350743926625e6312ab29b16a0c3e42
ivanov7as <96878229+ivanov7as@users.noreply.github.com>
2023-02-07 02:22:12 +0300

    Update tree.java

--- tree.java
+++ tree.java
@@ -1,1 +1,14 @@
+package FamilyTree;
 
+import java.util.List;
+
+public interface TreeNode {
+
+    public List<TreeNode> getParent();
+
+    public List<TreeNode> getChildren();
+
+    public void setParent(TreeNode parent);
+
+    public void addChildren(TreeNode children);
+}
